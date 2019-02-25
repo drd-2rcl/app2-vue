bfe405e8a88d7f34d912123fe211545e1cea1c5f
drd-2rcl <drd.rclrd@gmail.com>
2019-02-25 15:41:19 -0300

    :recycle: Refactor to communication between sibling components

--- src/components/User.vue
+++ src/components/User.vue
@@ -3,13 +3,24 @@
     <h1>The User Component</h1>
     <p>I'm an awesome User!</p>
     <button @click="changeName">Change my Name</button>
+    <p>Name is {{ name }} </p>
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <app-user-detail :name="name" ></app-user-detail>
+        <app-user-detail 
+          :myName="name" 
+          @nameWasReset="name=$event" 
+          :resetFn="resetName"
+          :userAge="age"
+        >
+        </app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit 
+          :userAge="age"
+          @ageWasEdited="age=$event"
+        >
+        </app-user-edit>
       </div>
     </div>
   </div>
@@ -22,12 +33,16 @@ import UserEdit from './UserEdit.vue';
 export default {
   data() {
     return {
-      name: 'Diogo'
+      name: 'Diogo',
+      age: 26
     };
   },
   methods: {
     changeName() {
       this.name = 'Judite';
+    },
+    resetName() {
+      this.name = 'Diogo';
     }
   },
   components: {
